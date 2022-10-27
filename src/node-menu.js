4dac6cb0207ebfbbb0d19020e6eac303616419ac
alaminjwel <jwel.cse@gmail.com>
2022-10-27 15:38:13 +0600

    initial

---
+++ src/node-menu.js
@@ -0,0 +1,33 @@
+import Menu from './menu/index';
+import { createNode, traverse } from './utils';
+
+export default class NodeMenu extends Menu {
+    constructor(editor, props, vueComponent, nodeItems) {
+        super(editor, props, vueComponent);
+
+        if (nodeItems['Delete'] !== false) {
+            this.addItem('Delete', ({ node }) => {
+                if (editor.selected.list.indexOf(node) !== -1) {
+                    editor.selected.remove(node);
+                }
+
+                editor.trigger('nodedelete', { node });
+
+                editor.removeNode(node);
+            });
+        }
+        if (nodeItems['Clone'] !== false) {
+            this.addItem('Clone', async (args) => {
+                const { name, position: [x, y], ...params } = args.node;
+                const component = editor.components.get(name);
+                const node = await createNode(component, { ...params, x: x + 10, y: y + 10 });
+
+                editor.trigger('nodeclone', { node, x, y });
+
+                editor.addNode(node);
+            });
+        }
+
+        traverse(nodeItems, (name, func, path) => this.addItem(name, func, path))
+    }
+}
